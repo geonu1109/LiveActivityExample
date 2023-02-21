d5442317e73920dc62239b755d1841cb63456385
luke.pkg <luke.pkg@kakaomobility.com>
2023-02-21 11:30:49 +0900

    Initial Commit

---
+++ LiveActivityExample/LiveActivityExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityExampleApp.swift
+//  LiveActivityExample
+//
+//  Created by luke.pkg on 2023/02/21.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
